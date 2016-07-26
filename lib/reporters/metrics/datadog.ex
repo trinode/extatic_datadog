f4e34c3966999eec63bbeee5715c66a6f8300f11
Anthony Graham <github@trinode.co.uk>
2016-07-26 10:49:58 +0100

    test

--- lib/reporters/metrics/datadog.ex
+++ lib/reporters/metrics/datadog.ex
@@ -1,6 +1,8 @@
 defmodule Extatic.Reporters.Metrics.Datadog do
   @behaviour Extatic.Behaviours.MetricReporter
   def send(stat_list) do
+    IO.puts "sending metrics"
+    IO.inspect stat_list
     send_request(stat_list)
   end
 
